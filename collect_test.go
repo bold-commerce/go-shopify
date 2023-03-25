d55d54fd81dc8821f97a6f349b64af13c8c3323b
Oliver <oliver006@users.noreply.github.com>
2023-03-25 10:04:57 -0400

    Update collect_test.go

--- collect_test.go
+++ collect_test.go
@@ -20,7 +20,7 @@ func collectTests(t *testing.T, collect Collect) {
 		{"CollectionID", int64(241600835), collect.CollectionID},
 		{"ProductID", int64(6654094787), collect.ProductID},
 		{"Featured", false, collect.Featured},
-		{"SortValue", "0000000001", collect.SortValue},
+		{"SortValue", "0000000002", collect.SortValue},
 	}
 
 	for _, c := range cases {
